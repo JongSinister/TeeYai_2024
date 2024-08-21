e1b42cb6ae5d7d9194601d1740d644de8f14fb83
Jeerasak Kajonrattanavanit <mazuroze@gmail.com>
2024-08-21 20:47:15 +0700

    add tailwind

--- FrontEnd/src/app/page.tsx
+++ FrontEnd/src/app/page.tsx
@@ -3,93 +3,11 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <>
+    <h1 className="text-4xl">Home Page</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. In hic cupiditate voluptatum rerum reiciendis tempora autem sunt quaerat. Neque magni quis veritatis illo cupiditate non! Hic aliquam nesciunt necessitatibus tenetur!
+    Maxime quo dolor qui amet? Enim quae perspiciatis sapiente doloribus cum ducimus libero consequuntur necessitatibus neque a assumenda reprehenderit culpa ipsa, minima ullam, aspernatur nemo quis! Molestias nostrum voluptate aut.
+    Dolorem architecto sint, sed quas nostrum esse natus harum reprehenderit autem blanditiis adipisci. Iste, illo necessitatibus. Accusantium numquam maiores rerum dolorum veniam, at officiis dignissimos, et quasi in ut blanditiis.</p>
+    </>
   );
 }
