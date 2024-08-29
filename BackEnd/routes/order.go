e9da3ae3e8540450f4a66009990aff579bf0340f
Jeerasak Kajonrattanavanit <mazuroze@gmail.com>
2024-08-29 16:18:26 +0700

    update protect

--- BackEnd/routes/order.go
+++ BackEnd/routes/order.go
@@ -2,15 +2,18 @@ package routes
 
 import (
 	"github.com/JongSinister/TeeYai_2024/controllers"
-	"github.com/JongSinister/TeeYai_2024/middleware"
 	"github.com/gofiber/fiber/v2"
 )
 
 func OrderRoutes(router fiber.Router) {
-	router.Use(middleware.Protect)
+	// router.Use(middleware.Protect)
 
-	router.Get("/", middleware.Protect, middleware.Authorize("admin"), controllers.GetOrders)
-	router.Post("/", middleware.Protect, controllers.AddOrder)
-	router.Get("/:id", middleware.Protect, controllers.GetOrder)
-	router.Delete("/:id", middleware.Protect, middleware.Authorize("admin"), controllers.DeleteOrder)
+	// router.Get("/", middleware.Protect, middleware.Authorize("admin"), controllers.GetOrders)
+	// router.Post("/", middleware.Protect, controllers.AddOrder)
+	// router.Get("/:id", middleware.Protect, controllers.GetOrder)
+	// router.Delete("/:id", middleware.Protect, middleware.Authorize("admin"), controllers.DeleteOrder)
+	router.Get("/", controllers.GetOrders)
+	router.Post("/", controllers.AddOrder)
+	router.Get("/:id", controllers.GetOrder)
+	router.Delete("/:id", controllers.DeleteOrder)
 }
--- BackEnd/routes/routes.go
+++ BackEnd/routes/routes.go
@@ -1,19 +1,18 @@
 package routes
 
 import (
-	"github.com/JongSinister/TeeYai_2024/middleware"
 	"github.com/gofiber/fiber/v2"
 )
 
 func Setup(app *fiber.App) {
-    // Set up routes for hospitals and patients
-    api := app.Group("/api/v1")
+	// Set up routes for hospitals and patients
+	api := app.Group("/api/v1")
 
-    // Set up routes for authentication
-    AuthRoutes(api.Group("/auth"))
+	// Set up routes for authentication
+	AuthRoutes(api.Group("/auth"))
 
-    protected := api.Group("/")
-    protected.Use(middleware.Protect)
-    // Set up routes for orders
-    OrderRoutes(api.Group("/orders"))
+	// protected := api.Group("/")
+	// protected.Use(middleware.Protect)
+	// Set up routes for orders
+	OrderRoutes(api.Group("/orders"))
 }
